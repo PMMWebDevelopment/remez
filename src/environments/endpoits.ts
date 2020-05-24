39643d14d8ef61a6c5161532dbf0cecf04c7943c
P M Meddings <pmmeddings@gmail.com>
2020-05-24 16:51:25 +0100

    Add files via upload

---
+++ src/environments/endpoits.ts
@@ -0,0 +1,9 @@
+export const endpoints = {
+    getLettersURL: 'https://remez-pmm2.firebaseio.com/writing.json',
+    getLetterCommentaryURL: 'https://pastebin.com/raw/',
+    getGrammarPointsURL: 'https://remez-pmm2.firebaseio.com/grammar.json',
+    getGrammarPointCommentaryURL: 'https://pastebin.com/raw/',
+    getGrammarPointExamplesURL: 'https://remez-pmm2.firebaseio.com/sentences.json',
+    getVerbsURL: 'https://remez-pmm2.firebaseio.com/verblist.json',
+    getVerbConjugationsURL: 'https://remez-pmm2.firebaseio.com/verbconjugations.json',
+}
